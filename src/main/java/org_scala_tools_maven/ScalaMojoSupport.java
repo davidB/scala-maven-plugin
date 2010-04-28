850449084362e5fa9159202f4c2bd67b213b1292
Joshua.Suereth@gmail.com <jsuereth@jsuereth-laptop.(none)>
2010-04-28 16:54:03 -0400

    Fixed issue where compiler plugins were not sent to the console

--- src/main/java/org_scala_tools_maven/ScalaMojoSupport.java
+++ src/main/java/org_scala_tools_maven/ScalaMojoSupport.java
@@ -493,7 +493,7 @@ abstract class ScalaMojoSupport extends AbstractMojo {
      * @param scalac
      * @throws Exception
      */
-    private void addCompilerPluginOptions(JavaMainCaller scalac) throws Exception {
+    protected void addCompilerPluginOptions(JavaMainCaller scalac) throws Exception {
         for (String plugin : getCompilerPlugins()) {
             scalac.addArgs("-Xplugin:" + plugin);
         }
