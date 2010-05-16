a05a1912cdea47fc30b8121c172e3075db844549
David Bernard <david.bernard.31@gmail.com>
2010-05-16 19:43:29 +0200

    bind add-sources to initialize instead of generate-sources

--- src/main/java/org_scala_tools_maven/AddSourceMojo.java
+++ src/main/java/org_scala_tools_maven/AddSourceMojo.java
@@ -11,50 +11,50 @@ import org.apache.maven.project.MavenProject;
  *
  * @executionStrategy always
  * @goal add-source
- * @phase generate-sources
+ * @phase initialize
  * @requiresDirectInvocation false
  */
 public class AddSourceMojo extends AbstractMojo {
 
-	/**
-	 * @parameter expression="${project}"
-	 * @required
-	 * @readonly
-	 */
-	private MavenProject project;
+    /**
+     * @parameter expression="${project}"
+     * @required
+     * @readonly
+     */
+    private MavenProject project;
 
     /**
      * The directory in which scala source is found
-     * 
+     *
      * @parameter expression="${project.build.sourceDirectory}/../scala"
      */
     protected File sourceDir;
 
     /**
      * The directory in which testing scala source is found
-     * 
+     *
      * @parameter expression="${project.build.testSourceDirectory}/../scala"
      */
     protected File testSourceDir;
 
     public void execute() throws MojoExecutionException {
-    	try {
-	    	if (sourceDir != null) {
-	        	String path = sourceDir.getCanonicalPath();
-	        	if (!project.getCompileSourceRoots().contains(path)) {
-	        		getLog().info("Add Source directory: " + path);
-	        		project.addCompileSourceRoot(path);
-	        	}
-			}
-	    	if (testSourceDir != null) {
-	        	String path = testSourceDir.getCanonicalPath();
-	        	if (!project.getTestCompileSourceRoots().contains(path)) {
-	        		getLog().info("Add Test Source directory: " + path);
-	        		project.addTestCompileSourceRoot(path);
-	        	}
-			}
-    	} catch(Exception exc) {
-    		getLog().warn(exc);
-    	}
-	}
+        try {
+            if (sourceDir != null) {
+                String path = sourceDir.getCanonicalPath();
+                if (!project.getCompileSourceRoots().contains(path)) {
+                    getLog().info("Add Source directory: " + path);
+                    project.addCompileSourceRoot(path);
+                }
+            }
+            if (testSourceDir != null) {
+                String path = testSourceDir.getCanonicalPath();
+                if (!project.getTestCompileSourceRoots().contains(path)) {
+                    getLog().info("Add Test Source directory: " + path);
+                    project.addTestCompileSourceRoot(path);
+                }
+            }
+        } catch(Exception exc) {
+            getLog().warn(exc);
+        }
+    }
 }
