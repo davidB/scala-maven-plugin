9310def21e7c560b929a75143e2a32874cd61e61
Peter Vlugter <pvlugter@gmail.com>
2012-06-19 17:08:23 +1200

    Integrate with zinc
    - use zinc as a library for incremental compile
    - support compile via zinc server

--- src/main/java/sbt_inc/SbtIncrementalCompiler.java
+++ src/main/java/sbt_inc/SbtIncrementalCompiler.java
@@ -1,20 +1,16 @@
 package sbt_inc;
 
+import com.typesafe.zinc.Compiler;
+import com.typesafe.zinc.Inputs;
+import com.typesafe.zinc.Setup;
+import com.typesafe.zinc.ZincClient;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.HashMap;
 import java.util.Map;
 import org.apache.maven.plugin.logging.Log;
-import sbt.compiler.AnalyzingCompiler;
-import sbt.compiler.CompilerCache;
-import sbt.compiler.CompilerCache$;
-import sbt.compiler.IC;
-import sbt.inc.Analysis;
-import xsbti.compile.CompileOrder;
-import xsbti.compile.DefinesClass;
-import xsbti.compile.GlobalsCache;
-import xsbti.Maybe;
+import scala_maven_executions.MainHelper;
 
 public class SbtIncrementalCompiler {
 
@@ -24,145 +20,126 @@ public class SbtIncrementalCompiler {
     public static final String COMPILER_INTERFACE_CLASSIFIER = "sources";
     public static final String XSBTI_ARTIFACT_ID = "sbt-interface";
 
+    private Log log;
+
+    private ZincClient zinc;
+
+    private boolean useServer = false;
+
+    private File compilerJar;
+
+    private File libraryJar;
+
+    private List<File> extraJars;
+
     private xsbti.Logger logger;
 
-    private SbtCompilers compilers;
+    private Compiler compiler;
 
-    private GlobalsCache compilerCache;
-
-    public SbtIncrementalCompiler(String scalaVersion, File libraryJar, File compilerJar, String sbtVersion, File xsbtiJar, File interfaceJar, int maxCompilers, Log log) throws Exception {
-        log.info("Using incremental recompilation");
-        this.logger = new SbtLogger(log);
-        this.compilers = new SbtCompilers(scalaVersion, libraryJar, compilerJar, sbtVersion, xsbtiJar, interfaceJar, logger);
-        this.compilerCache = (maxCompilers <= 0) ? CompilerCache.fresh() : CompilerCache$.MODULE$.apply(maxCompilers);
-    }
-
-    public void compile(List<String> classpathElements, List<File> sourcesList, File classesDirectory, List<String> scalacOptions, List<String> javacOptions, File cacheFile, Map<File, File> cacheMap) {
-        List<File> fullClasspath = pathsToFiles(classpathElements);
-        File[] classpath = fullClasspath.toArray(new File[fullClasspath.size()]);
-        File[] sources = sourcesList.toArray(new File[sourcesList.size()]);
-        String[] soptions = scalacOptions.toArray(new String[scalacOptions.size()]);
-        String[] joptions = javacOptions.toArray(new String[javacOptions.size()]);
-        Options options = new Options(classpath, sources, classesDirectory, soptions, joptions);
-        cacheFile = (cacheFile != null) ? cacheFile : SbtAnalysis.fallbackCacheLocation(classesDirectory);
-        Setup setup = new Setup(fullClasspath, classesDirectory, cacheFile, cacheMap, compilerCache);
-        Inputs inputs = new Inputs(compilers, options, setup);
-        Analysis analysis = IC.compile(inputs, logger);
-        SbtAnalysis.put(cacheFile, analysis);
-    }
-
-    public static class Options implements xsbti.compile.Options {
-
-        private File[] classpath;
-        private File[] sources;
-        private File classesDirectory;
-        private String[] scalacOptions;
-        private String[] javacOptions;
-
-        public Options(File[] classpath, File[] sources, File classesDirectory, String[] scalacOptions, String[] javacOptions) {
-            this.classpath = classpath;
-            this.sources = sources;
-            this.classesDirectory = classesDirectory;
-            this.scalacOptions = scalacOptions;
-            this.javacOptions = javacOptions;
-        }
-
-        public File[] classpath() {
-            return classpath;
-        }
-
-        public File[] sources() {
-            return sources;
-        }
-
-        public File classesDirectory() {
-            return classesDirectory;
-        }
-
-        public String[] options() {
-            return scalacOptions;
-        }
-
-        public String[] javacOptions() {
-            return javacOptions;
-        }
-
-        public int maxErrors() {
-            return 100;
-        }
-
-        public CompileOrder order() {
-            return CompileOrder.Mixed;
-        }
-    }
-
-    public static class Setup implements xsbti.compile.Setup<Analysis> {
-
-        private File cacheFile;
-        private GlobalsCache compilerCache;
-        private HashMap<File, Maybe<Analysis>> analysisCache;
-
-        public Setup(List<File> fullClasspath, File classesDirectory, File cacheFile, Map<File, File> cacheMap, GlobalsCache compilerCache) {
-            this.cacheFile = cacheFile;
-            this.compilerCache = compilerCache;
-            this.analysisCache = new HashMap<File, Maybe<Analysis>>();
-            for (File file : fullClasspath) {
-                analysisCache.put(file, SbtAnalysis.getAnalysis(file, classesDirectory, cacheMap));
+    public SbtIncrementalCompiler(boolean useZincServer, int zincPort, String scalaVersion, File libraryJar, File compilerJar, List<File> extraJars, String sbtVersion, File xsbtiJar, File interfaceJar, Log log) throws Exception {
+        this.log = log;
+        if (useZincServer) {
+            this.zinc = new ZincClient(zincPort);
+            if (zinc.serverAvailable()) {
+                log.info("Using zinc server for incremental compilation");
+                this.useServer = true;
+                this.compilerJar = compilerJar;
+                this.libraryJar = libraryJar;
+                this.extraJars = extraJars;
+            } else {
+                log.warn("Zinc server is not available at port " + zincPort + " - reverting to normal incremental compile");
+                this.useServer = false;
             }
         }
-
-        public Maybe<Analysis> analysisMap(File file) {
-            Maybe<Analysis> analysis = analysisCache.get(file);
-            return (analysis == null) ? SbtAnalysis.JUST_EMPTY_ANALYSIS : analysis;
-        }
-
-        public DefinesClass definesClass(File file) {
-            return SbtLocate.definesClass(file);
-        }
-
-        public boolean skip() {
-            return false;
-        }
-
-        public File cacheFile() {
-            return cacheFile;
-        }
-
-        public GlobalsCache cache() {
-            return compilerCache;
+        if (!useServer) {
+            log.info("Using incremental compilation");
+            this.logger = new SbtLogger(log);
+            Setup setup = Setup.create(compilerJar, libraryJar, extraJars, xsbtiJar, interfaceJar, null);
+            if (log.isDebugEnabled()) Setup.debug(setup, logger);
+            this.compiler = Compiler.create(setup, logger);
         }
     }
 
-    public static class Inputs implements xsbti.compile.Inputs<Analysis, AnalyzingCompiler> {
-
-        private SbtCompilers compilers;
-        private Options options;
-        private Setup setup;
-
-        public Inputs(SbtCompilers compilers, Options options, Setup setup) {
-            this.compilers = compilers;
-            this.options = options;
-            this.setup = setup;
-        }
-
-        public xsbti.compile.Compilers<AnalyzingCompiler> compilers() {
-            return compilers;
-        }
-
-        public xsbti.compile.Options options() {
-            return options;
-        }
-
-        public xsbti.compile.Setup<Analysis> setup() {
-            return setup;
+    public void compile(File baseDir, List<String> classpathElements, List<File> sources, File classesDirectory, List<String> scalacOptions, List<String> javacOptions, File cacheFile, Map<File, File> cacheMap, String compileOrder) throws Exception {
+        if (useServer) {
+            zincCompile(baseDir, classpathElements, sources, classesDirectory, scalacOptions, javacOptions, cacheFile, cacheMap, compileOrder);
+        } else {
+            if (log.isDebugEnabled()) log.debug("Incremental compiler = " + compiler + " [" + Integer.toHexString(compiler.hashCode()) + "]");
+            List<File> classpath = pathsToFiles(classpathElements);
+            Inputs inputs = Inputs.create(classpath, sources, classesDirectory, scalacOptions, javacOptions, cacheFile, cacheMap, compileOrder);
+            if (log.isDebugEnabled()) Inputs.debug(inputs, logger);
+            compiler.compile(inputs, logger);
         }
     }
 
-    public List<File> pathsToFiles(List<String> paths) {
+    private void zincCompile(File baseDir, List<String> classpathElements, List<File> sources, File classesDirectory, List<String> scalacOptions, List<String> javacOptions, File cacheFile, Map<File, File> cacheMap, String compileOrder) throws Exception {
+        List<String> arguments = new ArrayList<String>();
+        arguments.add("-log-level");
+        arguments.add(logLevelToString(log));
+        arguments.add("-scala-compiler");
+        arguments.add(compilerJar.getAbsolutePath());
+        arguments.add("-scala-library");
+        arguments.add(libraryJar.getAbsolutePath());
+        arguments.add("-scala-extra");
+        List<String> extraPaths = new ArrayList<String>();
+        for (File extraJar : extraJars) {
+            extraPaths.add(extraJar.getAbsolutePath());
+        }
+        arguments.add(MainHelper.toMultiPath(extraPaths));
+        arguments.add("-classpath");
+        arguments.add(MainHelper.toMultiPath(classpathElements));
+        arguments.add("-d");
+        arguments.add(classesDirectory.getAbsolutePath());
+        for (String scalacOption : scalacOptions) {
+            arguments.add("-S" + scalacOption);
+        }
+        for (String javacOption : javacOptions) {
+            arguments.add("-J" + javacOption);
+        }
+        arguments.add("-compile-order");
+        arguments.add(compileOrder);
+        arguments.add("-analysis-cache");
+        arguments.add(cacheFile.getAbsolutePath());
+        arguments.add("-analysis-map");
+        arguments.add(cacheMapToString(cacheMap));
+        for (File source : sources) {
+            arguments.add(source.getAbsolutePath());
+        }
+
+        int exitCode = zinc.run(arguments, baseDir, System.out, System.err);
+
+        if (exitCode != 0) {
+            xsbti.Problem[] problems = null;
+            throw new sbt.compiler.CompileFailed(arguments.toArray(new String[arguments.size()]), "Compile failed via zinc server", problems);
+        }
+    }
+
+    private List<File> pathsToFiles(List<String> paths) {
         List<File> files = new ArrayList<File>(paths.size());
         for (String path : paths) {
             files.add(new File(path));
         }
         return files;
+    }
+
+    private String logLevelToString(Log log) {
+        if (log.isDebugEnabled()) return "debug";
+        else if (log.isInfoEnabled()) return "info";
+        else if (log.isWarnEnabled()) return "warn";
+        else if (log.isErrorEnabled()) return "error";
+        else return "info";
+    }
+
+    private String cacheMapToString(Map<File, File> cacheMap) throws Exception {
+        String analysisMap = "";
+        boolean addComma = false;
+        for (Map.Entry<File, File> entry : cacheMap.entrySet()) {
+            if (addComma) analysisMap += ",";
+            analysisMap += entry.getKey().getAbsolutePath();
+            analysisMap += ":";
+            analysisMap += entry.getValue().getAbsolutePath();
+            addComma = true;
+        }
+        return analysisMap;
     }
 }
