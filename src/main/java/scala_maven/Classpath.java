f3f37bbb9f184d53d52177f62a34ae71fe0a18b7
Carlos Rodrigues <cmsrodrigues@gmail.com>
2019-02-17 11:23:11 +0000

    Issue #318 Refactoring
    Signed-off-by: Carlos Rodrigues <cmsrodrigues@gmail.com>

---
+++ src/main/java/scala_maven/Classpath.java
@@ -0,0 +1,16 @@
+package scala_maven;
+
+import java.io.File;
+import java.util.List;
+
+public class Classpath {
+    private List<File> add;
+
+    public List<File> getAdd() {
+        return add;
+    }
+
+    public void setAdd(List<File> add) {
+        this.add = add;
+    }
+}
