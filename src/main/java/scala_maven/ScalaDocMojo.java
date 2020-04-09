davidB/scala-maven-plugin

commit d9f45d0dae6955b459367b11f97305d2ec2bad75

Migrated javadoc-style annotation to Maven Plugin Annotations.
The two main reasons for this move:

1. It is partially checked by the compiler

2. It makes interoperability easier, as the Java annotations were preserved in the classfiles whereas the javadoc-annoations were not.
E.g. The bloop project has a maven plugin which inherits from some base classes of this plugin to read it's configuration.
After this change, the maven plugin build just works, but before, it couldn't see any @parameter and @component comments, which was a real pain.

src/main/java/scala_maven/ScalaDocMojo.java
@@ -1,35 +1,39 @@
 package scala_maven;
 
-import org.apache.maven.project.MavenProject;
-import org.apache.maven.reporting.MavenReport;
-import org.apache.maven.reporting.MavenReportException;
-import org.codehaus.doxia.sink.Sink;
-import org.codehaus.plexus.util.StringUtils;
-import scala_maven_executions.JavaMainCaller;
-import scala_maven_executions.MainHelper;
-
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 import java.util.Locale;
 
+import org.apache.maven.plugins.annotations.Execute;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.ResolutionScope;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.reporting.MavenReport;
+import org.apache.maven.reporting.MavenReportException;
+import org.codehaus.doxia.sink.Sink;
+import org.codehaus.plexus.util.StringUtils;
+
+import scala_maven_executions.JavaMainCaller;
+import scala_maven_executions.MainHelper;
+
 /**
  * Produces Scala API documentation.
  *
- * @goal doc
- * @requiresDependencyResolution compile
- * @execute phase="generate-sources"
  */
+@Mojo(name = "doc", requiresDependencyResolution = ResolutionScope.COMPILE)
+@Execute(phase = LifecyclePhase.GENERATE_RESOURCES)
 public class ScalaDocMojo extends ScalaSourceMojoSupport implements MavenReport {
 
     /**
      * Specify window title of generated HTML documentation.
      * [scaladoc, vscaladoc]
      *
-     * @parameter property="windowtitle"
-     *            default-value="${project.name} ${project.version} API"
      */
+    @Parameter(property = "windowtitle", defaultValue = "${project.name} ${project.version} API")
     protected String windowtitle;
 
     /**
@@ -39,144 +43,143 @@ public class ScalaDocMojo extends ScalaSourceMojoSupport implements MavenReport 
      * href="http://www.mycompany.com">MyCompany, Inc.&lt;a>]]&gt;
      * [scaladoc, vscaladoc]
      *
-     * @parameter property="bottom"
-     *            default-value="Copyright (c) {inceptionYear}-{currentYear} {organizationName}. All Rights Reserved."
      */
+    @Parameter(property = "bottom", defaultValue = "Copyright (c) {inceptionYear}-{currentYear} {organizationName}. All Rights Reserved.")
     protected String bottom;
 
     /**
      * Charset for cross-platform viewing of generated documentation.
      * [scaladoc, vscaladoc]
      *
-     * @parameter property="charset" default-value="ISO-8859-1"
      */
+    @Parameter(property = "charset", defaultValue = "ISO-8859-1")
     protected String charset;
 
     /**
      * Include title for the overview page.
      * [scaladoc, scaladoc2, vscaladoc]
      *
-     * @parameter property="doctitle"
-     *            default-value="${project.name} ${project.version} API"
      */
+    @Parameter(property = "doctitle", defaultValue = "${project.name} ${project.version} API")
     protected String doctitle;
 
     /**
      * Include footer text for each page.
      * [scaladoc, vscaladoc]
      *
-     * @parameter property="footer"
      */
+    @Parameter(property = "footer")
     protected String footer;
 
     /**
      * Include header text for each page
      * [scaladoc, vscaladoc]
      *
-     * @parameter property="header"
      */
+    @Parameter(property = "header")
     protected String header;
 
     /**
      * Generate source in HTML
      * [scaladoc, vscaladoc]
      *
-     * @parameter property="linksource" default-value="true"
      */
+    @Parameter(property = "linksource", defaultValue = "true")
     protected boolean linksource;
 
     /**
      * Suppress description and tags, generate only declarations
      * [scaladoc, vscaladoc]
      *
-     * @parameter property="nocomment" default-value="false"
      */
+    @Parameter(property = "nocomment", defaultValue = "false")
     protected boolean nocomment;
 
     /**
      * File to change style of the generated documentation
      * [scaladoc, vscaladoc]
      *
-     * @parameter property="stylesheetfile"
      */
+    @Parameter(property = "stylesheetfile")
     protected File stylesheetfile;
 
     /**
      * Include top text for each page
      * [scaladoc, vscaladoc]
      *
-     * @parameter property="top"
      */
+    @Parameter(property = "top")
     protected String top;
 
     /**
      * Specifies the destination directory where scalaDoc saves the generated
      * HTML files.
      *
-     * @parameter default-value="scaladocs"
-     * @required
      */
+    @Parameter(defaultValue = "scaladocs", required = true)
     protected String outputDirectory;
 
     /**
-     * Specifies the destination directory where javadoc saves the generated HTML files.
+     * Specifies the destination directory where javadoc saves the generated HTML
+     * files.
      *
-     * @parameter default-value="${project.reporting.outputDirectory}/scaladocs"
-     * @required
      */
+    @Parameter(defaultValue = "${project.reporting.outputDirectory}/scaladocs", required = true)
     protected File reportOutputDirectory;
 
     /**
      * The name of the Scaladoc report.
      *
      * @since 2.1
-     * @parameter property="name" default-value="ScalaDocs"
      */
+    @Parameter(property = "name", defaultValue = "ScalaDocs")
     private String name;
 
     /**
      * The description of the Scaladoc report.
      *
      * @since 2.1
-     * @parameter property="description" default-value="ScalaDoc API
-     *            documentation."
      */
+    @Parameter(property = "description", defaultValue = "ScalaDoc API documentation.")
     private String description;
 
     /**
-     * className (FQN) of the main scaladoc to use, if not define, the the scalaClassName is used
+     * className (FQN) of the main scaladoc to use, if not define, the the
+     * scalaClassName is used
      *
-     * @parameter property="maven.scaladoc.className"
      */
+    @Parameter(property = "maven.scaladoc.className")
     protected String scaladocClassName;
 
     /**
-     * If you want to use vscaladoc to generate api instead of regular scaladoc, set the version of vscaladoc you want to use.
+     * If you want to use vscaladoc to generate api instead of regular scaladoc, set
+     * the version of vscaladoc you want to use.
      *
-     * @parameter property="maven.scaladoc.vscaladocVersion"
      */
+    @Parameter(property = "maven.scaladoc.vscaladocVersion")
     protected String vscaladocVersion;
 
     /**
-     * To allow running aggregation only from command line use "-DforceAggregate=true" (avoid using in pom.xml).
+     * To allow running aggregation only from command line use
+     * "-DforceAggregate=true" (avoid using in pom.xml).
      * [scaladoc, vscaladoc]
      *
-     * @parameter property="forceAggregate" default-value="false"
      */
+    @Parameter(property = "forceAggregate", defaultValue = "false")
     protected boolean forceAggregate = false;
 
     /**
      * If you want to aggregate only direct sub modules.
      *
-     * @parameter property="maven.scaladoc.aggregateDirectOnly" default-value="true"
      */
+    @Parameter(property = "maven.scaladoc.aggregateDirectOnly", defaultValue = "true")
     protected boolean aggregateDirectOnly = true;
 
     /**
      * The directory which contains scala/java source files
      *
-     * @parameter default-value="${project.build.sourceDirectory}/../scala"
      */
+    @Parameter(defaultValue = "${project.build.sourceDirectory}/../scala")
     protected File sourceDir;
 
     private List<File> _sourceFiles;

pom.xml
@@ -120,6 +120,12 @@
       <scope>provided</scope>
     </dependency>
     <dependency>
+      <groupId>org.apache.maven.plugin-tools</groupId>
+      <artifactId>maven-plugin-annotations</artifactId>
+      <version>3.5</version>
+      <scope>provided</scope>
+    </dependency>
+    <dependency>
       <groupId>org.apache.maven.reporting</groupId>
       <artifactId>maven-reporting-api</artifactId>
       <version>${maven.reporting.version}</version>

src/main/java/scala_maven/AddSourceMojo.java
@@ -4,67 +4,62 @@ import java.io.File;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
 
 /**
  * Add more source directories to the POM.
- *
- * @executionStrategy always
- * @goal add-source
- * @phase initialize
- * @requiresDirectInvocation false
- * @threadSafe
  */
+@Mojo(name = "add-source", executionStrategy = "always", defaultPhase = LifecyclePhase.INITIALIZE, requiresDirectInvocation = false, threadSafe = true)
 public class AddSourceMojo extends AbstractMojo {
 
     /**
      * The maven project
-     *
-     * @parameter property="project"
-     * @required
-     * @readonly
      */
+    @Parameter(property = "project", required = true, readonly = true)
     private MavenProject project;
 
     /**
      * The directory in which scala source is found
-     *
-     * @parameter default-value="${project.build.sourceDirectory}/../scala"
      */
+    @Parameter(defaultValue = "${project.build.sourceDirectory}/../scala")
     protected File sourceDir;
 
     /**
      * The directory in which testing scala source is found
-     *
-     * @parameter default-value="${project.build.testSourceDirectory}/../scala"
      */
+    @Parameter(defaultValue = "${project.build.testSourceDirectory}/../scala")
     protected File testSourceDir;
 
     /**
-     * Should use CanonicalPath to normalize path (true => getCanonicalPath, false => getAbsolutePath)
+     * Should use CanonicalPath to normalize path (true => getCanonicalPath, false
+     * => getAbsolutePath)
+     * 
      * @see https://github.com/davidB/maven-scala-plugin/issues/50
-     * @parameter property="maven.scala.useCanonicalPath" default-value="true"
      */
+    @Parameter(property = "maven.scala.useCanonicalPath", defaultValue = "true")
     protected boolean useCanonicalPath = true;
-    
+
     @Override
     public void execute() throws MojoExecutionException {
         try {
             if (sourceDir != null) {
-                String path = FileUtils.pathOf(sourceDir, useCanonicalPath);
+                final String path = FileUtils.pathOf(sourceDir, useCanonicalPath);
                 if (!project.getCompileSourceRoots().contains(path)) {
                     getLog().info("Add Source directory: " + path);
                     project.addCompileSourceRoot(path);
                 }
             }
             if (testSourceDir != null) {
-                String path = FileUtils.pathOf(testSourceDir, useCanonicalPath);
+                final String path = FileUtils.pathOf(testSourceDir, useCanonicalPath);
                 if (!project.getTestCompileSourceRoots().contains(path)) {
                     getLog().info("Add Test Source directory: " + path);
                     project.addTestCompileSourceRoot(path);
                 }
             }
-        } catch(Exception exc) {
+        } catch (final Exception exc) {
             getLog().warn(exc);
         }
     }

src/main/java/scala_maven/Launcher.java
@@ -1,5 +1,7 @@
 package scala_maven;
 
+import org.apache.maven.plugins.annotations.Parameter;
+
 public class Launcher {
     protected String id;
 
@@ -7,15 +9,13 @@ public class Launcher {
 
     /**
      * Jvm Arguments
-     *
-     * @parameter
      */
+    @Parameter
     protected String[] jvmArgs;
 
     /**
      * compiler additionnals arguments
-     *
-     * @parameter
      */
+    @Parameter
     protected String[] args;
 }

src/main/java/scala_maven/ScalaCompileMojo.java
@@ -1,41 +1,40 @@
- package scala_maven;
+package scala_maven;
 
 import java.io.File;
 import java.util.LinkedList;
 import java.util.List;
 
 import org.apache.maven.model.Dependency;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.ResolutionScope;
 
 /**
  * Compiles a directory of Scala source. Corresponds roughly to the compile goal
  * of the maven-compiler-plugin
  *
- * @phase compile
- * @goal compile
- * @requiresDependencyResolution compile
- * @threadSafe
  */
+@Mojo(name = "compile", defaultPhase = LifecyclePhase.COMPILE, requiresDependencyResolution = ResolutionScope.COMPILE, threadSafe = true)
 public class ScalaCompileMojo extends ScalaCompilerSupport {
 
     /**
      * The directory in which to place compilation output
-     *
-     * @parameter property="project.build.outputDirectory"
      */
+    @Parameter(property = "project.build.outputDirectory")
     protected File outputDir;
 
     /**
      * The directory which contains scala/java source files
-     *
-     * @parameter default-value="${project.build.sourceDirectory}/../scala"
      */
+    @Parameter(defaultValue = "${project.build.sourceDirectory}/../scala")
     protected File sourceDir;
 
     /**
      * Analysis cache file for incremental recompilation.
      *
-     * @parameter property="analysisCacheFile" default-value="${project.build.directory}/analysis/compile"
      */
+    @Parameter(property = "analysisCacheFile", defaultValue = "${project.build.directory}/analysis/compile")
     protected File analysisCacheFile;
 
     @Override

src/main/java/scala_maven/ScalaCompilerSupport.java
@@ -1,16 +1,23 @@
 package scala_maven;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
 import org.apache.maven.model.Plugin;
 import org.apache.maven.plugin.descriptor.PluginDescriptor;
+import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
 import org.codehaus.plexus.util.StringUtils;
 import org.codehaus.plexus.util.xml.Xpp3Dom;
+
 import sbt_inc.SbtIncrementalCompiler;
 import scala_maven_executions.JavaMainCaller;
 import scala_maven_executions.MainHelper;
-
-import java.io.File;
-import java.util.*;
 
 /**
  * Abstract parent of all Scala Mojo who run compilation
@@ -27,21 +34,25 @@ public abstract class ScalaCompilerSupport extends ScalaSourceMojoSupport {
     private boolean compileErrors;
 
     /**
-     * Recompile mode to use when sources were previously compiled and there is at least one change:
-     * "modified-only" => only modified sources are recompiled (pre 2.13 behavior), "all" => all sources are recompiled,
-     * "incremental" => incrementally recompile modified sources and other affected sources.
+     * Recompile mode to use when sources were previously compiled and there is at
+     * least one change:
+     * "modified-only" => only modified sources are recompiled (pre 2.13 behavior),
+     * "all" => all sources are recompiled,
+     * "incremental" => incrementally recompile modified sources and other affected
+     * sources.
      *
-     * @parameter property="recompileMode" default-value="all"
      */
+    @Parameter(property = "recompileMode", defaultValue = "all")
     protected String recompileMode = ALL;
 
     /**
      * notifyCompilation if true then print a message "path: compiling"
      * for each root directory or files that will be compiled.
-     * Useful for debug, and for integration with Editor/IDE to reset markers only for compiled files.
+     * Useful for debug, and for integration with Editor/IDE to reset markers only
+     * for compiled files.
      *
-     * @parameter property="notifyCompilation" default-value="true"
      */
+    @Parameter(property = "notifyCompilation", defaultValue = "true")
     private boolean notifyCompilation = true;
 
     abstract protected File getOutputDir() throws Exception;
@@ -62,29 +73,29 @@ public abstract class ScalaCompilerSupport extends ScalaSourceMojoSupport {
      *
      * Can be Mixed, JavaThenScala, or ScalaThenJava.
      *
-     * @parameter property="compileOrder" default-value="mixed"
      */
+    @Parameter(property = "compileOrder", defaultValue = "mixed")
     private String compileOrder;
 
     /**
      * Use zinc server for incremental recompilation.
-     *
-     * @parameter property="useZincServer" default-value="false"
      */
+    @Parameter(property = "useZincServer", defaultValue = "false")
     private boolean useZincServer;
 
     /**
      * Zinc server port, if running with incremental zinc server mode.
-     *
-     * @parameter property="zincPort" default-value="3030"
      */
+    @Parameter(property = "zincPort", defaultValue = "3030")
     private int zincPort;
 
     /**
      * Additional parameter to use to call zinc server
-     * It is a pipe '|' separated list of arguments, so it can be used from command line ("-DaddZincArgs=arg1|arg2|arg3|...").
-     * @parameter property="addZincArgs"
+     * It is a pipe '|' separated list of arguments, so it can be used from command
+     * line ("-DaddZincArgs=arg1|arg2|arg3|...").
+     * 
      */
+    @Parameter(property = "addZincArgs")
     private String addZincArgs = "";
 
     @Override

src/main/java/scala_maven/ScalaConsoleMojo.java
@@ -2,27 +2,23 @@ package scala_maven;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.LinkedHashSet;
-import java.util.List;
 import java.util.Set;
 
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.ResolutionScope;
+
 import scala_maven_executions.JavaMainCaller;
-import scala_maven_executions.JavaMainCallerInProcess;
 import scala_maven_executions.MainHelper;
 
-import org.apache.maven.artifact.Artifact;
-
 /**
- * Run the Scala console with all the classes of the projects (dependencies and builded)
+ * Run the Scala console with all the classes of the projects (dependencies and
+ * builded)
  *
- * @goal console
- * @requiresDependencyResolution test
- * @inheritByDefault false
- * @requiresDirectInvocation true
- * @executionStrategy once-per-session
  */
+@Mojo(name = "console", requiresDependencyResolution = ResolutionScope.TEST, inheritByDefault = false, requiresDirectInvocation = true, executionStrategy = "once-per-session")
 public class ScalaConsoleMojo extends ScalaMojoSupport {
 
     // Private Static Values //
@@ -44,33 +40,32 @@ public class ScalaConsoleMojo extends ScalaMojoSupport {
     /**
      * The console to run.
      *
-     * @parameter property="mainConsole" default-value="scala.tools.nsc.MainGenericRunner"
-     * @required
      */
+    @Parameter(property = "mainConsole", defaultValue = "scala.tools.nsc.MainGenericRunner", required = true)
     protected String mainConsole;
 
     /**
-     * Add the test classpath (include classes from test directory), to the console's classpath ?
+     * Add the test classpath (include classes from test directory), to the
+     * console's classpath ?
      *
-     * @parameter property="maven.scala.console.useTestClasspath" default-value="true"
-     * @required
      */
+    @Parameter(property = "maven.scala.console.useTestClasspath", defaultValue = "true", required = true)
     protected boolean useTestClasspath;
 
     /**
      * Add the runtime classpath, to the console's classpath ?
      *
-     * @parameter property="maven.scala.console.useRuntimeClasspath" default-value="true"
-     * @required
      */
+    @Parameter(property = "maven.scala.console.useRuntimeClasspath", defaultValue = "true", required = true)
     protected boolean useRuntimeClasspath;
 
     /**
      * Path of the javaRebel jar. If this option is set then the console run
-     * with <a href="http://www.zeroturnaround.com/javarebel/">javarebel</a> enabled.
+     * with <a href="http://www.zeroturnaround.com/javarebel/">javarebel</a>
+     * enabled.
      *
-     * @parameter property="javarebel.jar.path"
      */
+    @Parameter(property = "javarebel.jar.path")
     protected File javaRebelPath;
 
     @Override
@@ -87,7 +82,6 @@ public class ScalaConsoleMojo extends ScalaMojoSupport {
         }
 
         // Setup the classpath
-
 
         // Build the classpath string.
         final String classpathStr = MainHelper.toMultiPath(classpath.toArray(new String[classpath.size()]));

src/main/java/scala_maven/ScalaContinuousCompileMojo.java
@@ -4,77 +4,84 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.ResolutionScope;
+
 import scala_maven_executions.JavaMainCaller;
 
 /**
- * Compile the main and test scala source directory in continuous (infinite loop). !! This is an util goal for commandline usage only (Do not use or call it in a pom) !!!
+ * Compile the main and test scala source directory in continuous (infinite
+ * loop). !! This is an util goal for commandline usage only (Do not use or call
+ * it in a pom) !!!
  *
- * @goal cc
- * @requiresDependencyResolution test
  */
+@Mojo(name = "cc", requiresDependencyResolution = ResolutionScope.TEST)
 public class ScalaContinuousCompileMojo extends ScalaCompilerSupport {
 
     /**
      * The output directory for compilation.
      *
-     * @parameter property="project.build.outputDirectory"
      */
+    @Parameter(property="project.build.outputDirectory")
     protected File mainOutputDir;
 
     /**
      * The main directory containing scala source for compilation
      *
-     * @parameter default-value="${project.build.sourceDirectory}/../scala"
      */
+     @Parameter (defaultValue="${project.build.sourceDirectory}/../scala")
     protected File mainSourceDir;
 
     /**
      * The directory to place test compilation output in
      *
-     * @parameter default-value="${project.build.testOutputDirectory}
      */
+     @Parameter(defaultValue="${project.build.testOutputDirectory}")
     protected File testOutputDir;
 
     /**
      * The directory containing test source for compilation
      *
-     * @parameter default-value="${project.build.testSourceDirectory}/../scala"
      */
+     @Parameter (defaultValue="${project.build.testSourceDirectory}/../scala")
     protected File testSourceDir;
 
     /**
      * Analysis cache file for incremental recompilation.
      *
-     * @parameter property="analysisCacheFile" default-value="${project.build.directory}/analysis/compile"
      */
+     @Parameter (property="analysisCacheFile", defaultValue="${project.build.directory}/analysis/compile")
     protected File analysisCacheFile;
 
     /**
      * Analysis cache file for incremental recompilation.
      *
-     * @parameter property="testAnalysisCacheFile" default-value="${project.build.directory}/analysis/test-compile"
      */
+     @Parameter (property="testAnalysisCacheFile", defaultValue="${project.build.directory}/analysis/test-compile")
     protected File testAnalysisCacheFile;
 
     /**
      * Define if fsc should be used, else scalac is used.
      * fsc => scala.tools.nsc.CompileClient, scalac => scala.tools.nsc.Main.
      *
-     * @parameter property="fsc" default-value="true"
      */
+     @Parameter(property="fsc", defaultValue="true")
     protected boolean useFsc = true;
 
     /**
-     * Define if cc should run once or in infinite loop. (useful for test or working with editor)
-     * @parameter property="once" default-value="false"
+     * Define if cc should run once or in infinite loop. (useful for test or working
+     * with editor)
+     * 
      */
+     @Parameter(property="once", defaultValue="false")
     protected boolean once = false;
 
     /**
      * Turns verbose output on.
      *
-     * @parameter property="verbose" default-value="false"
      */
+     @Parameter(property="verbose", defaultValue="false")
     protected boolean verbose = false;
 
     @Override

commit b251549aabc89092d2dd918610bfde97129cb3e5

ScalaDocMojo should execute generate-sources, not generate-resources, close #397
Motivation:

scala:doc should work the same way as javadoc:javadoc and execute `generate-sources` phase.

https://github.com/davidB/scala-maven-plugin/commit/d9f45d0dae6955b459367b11f97305d2ec2bad75 introduced a regression when migrating to Java 5 annotations.

Modification:

Fix ScalaDocMojo to execute LifecyclePhase.GENERATE_SOURCES.

Result:

scala:doc executes correct maven lifecycle phase.

src/main/java/scala_maven/ScalaDocMojo.java
@@ -25,7 +25,7 @@ import scala_maven_executions.MainHelper;
  *
  */
 @Mojo(name = "doc", requiresDependencyResolution = ResolutionScope.COMPILE)
-@Execute(phase = LifecyclePhase.GENERATE_RESOURCES)
+@Execute(phase = LifecyclePhase.GENERATE_SOURCES)
 public class ScalaDocMojo extends ScalaSourceMojoSupport implements MavenReport {
 
     /**