95a02e9973e5939e078459a8cbe1caec0f85b306
Stephane Landelle <slandelle@gatling.io>
2020-11-15 14:49:00 +0100

    Drop IT tests I can't make sense of

--- src/it/test_script_on_non_scala_project/build.scala
+++
@@ -1,8 +0,0 @@
-import java.io.PrintWriter
-println("hello from build.scala ")
-val fo = new PrintWriter("target/hello-from-external.txt")
-try {
-  fo.println("Hello")
-} finally {
-  fo.close()
-}
