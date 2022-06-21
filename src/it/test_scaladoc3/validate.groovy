22d453d36e718fdf38bc76c0fd3045af00f1602b
Christof Reichardt <chr78rm@onlinehome.de>
2022-06-21 15:03:01 +0200

    Formatting.

--- src/it/test_scaladoc3/validate.groovy
+++ src/it/test_scaladoc3/validate.groovy
@@ -1,9 +1,8 @@
 try {
+  def file = new File(basedir, 'target/scaladoc-jar-test-1.0-SNAPSHOT-javadoc.jar')
+  assert file.exists()
 
-def file = new File(basedir, 'target/scaladoc-jar-test-1.0-SNAPSHOT-javadoc.jar')
-assert file.exists()
-
-return true
+  return true
 
 } catch(Throwable e) {
   e.printStackTrace()
